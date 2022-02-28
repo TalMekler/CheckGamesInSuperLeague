4a5c2c077d3adf1319d18b6ecf34347abc387d59
TalMekler <tal.mekler11@gmail.com>
2022-02-28 18:19:34 +0200

    not working yet

--- Ligat_Al/check_infi/Main.c
+++ Ligat_Al/check_infi/Main.c
@@ -1,10 +1,19 @@
 
-// branch recognize
-
 #include "Team.h"
 
+// rec branch
 int main() {
-	printf("In branch rec \n");
 
+	Team t0 = { 0, 0 }; // natania 0
+	Team t1 = { 1, 0 }; // hadera 1
+	Team t2 = { 2, 0 }; // saknin 2
+	Team t3 = { 3, 0 }; // hapoel 3
+	Team t4 = { 4, 0 }; // kiryat shmona
+	Team t_arr[] = { t0,t1,t2,t3,t4 };
+	int arr[GAMES] = { 0 };
+	func(arr, GAMES, t_arr);
+	printf(">>> Games remined: %d <<<\n", GAMES);
+	printf(">>> There are %d/%d combinations <<<\n", cnt_opt, total_opt);
 	return 0;
+
 }
--- Ligat_Al/check_infi/Team.h
+++ Ligat_Al/check_infi/Team.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef TEAM_DEBUG
+#define TEAM_DEBUG 1
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,9 +9,29 @@
 #define TEAMS 5
 #define MAX_TEAM_IN_PLAYOFF 3
 
-enum teams { Nat = 0, Had, Sak, Hap, KS };
-const char* teams_name[] = { "Natania", "Hadera", "Saknin", "Hapoel", "Kiryat Shmona" };
-
+#ifndef TEAM_DEBUG2
+#define TEAM_DEBUG2 1
 // Global Vars
 int cnt_opt = 0, total_opt = 0;
+enum teams { Nat = 0, Had, Sak, Hap, KS };
+const char* teams_name[] = { "Natania", "Hadera", "Saknin", "Hapoel", "Kiryat Shmona" };
+#endif // !TEAM_DEBUG2
+
+
+
+#endif // !TEAM_DEBUG
+
+typedef struct {
+	int id;
+	int points;
+} Team;
+
+void printArr(int* arr, int size);
+void func(int* arr, int size, Team* teams);
+void calcPoints(int* recursion_arr, Team* teams, int size);
+void printTeams(Team* teams, int size, int* recursion_arr, int size2);
+Team* sortTeamsByPoints(Team* teams, int size);
+void printGames(int* recursion_arr);
+Team cpyTeam(Team* t);
+int isInPlayoff(Team* sorted, int team_id);
 

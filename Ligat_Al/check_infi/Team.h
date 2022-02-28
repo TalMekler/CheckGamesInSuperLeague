4842429b0afc792a096ca9da9be6d49a65362895
TalMekler <tal.mekler11@gmail.com>
2022-02-28 17:56:10 +0200

    .

--- Ligat_Al/check_infi/Team.h
+++ Ligat_Al/check_infi/Team.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GAMES 7
+#define TEAMS 5
+#define MAX_TEAM_IN_PLAYOFF 3
+
+enum teams { Nat = 0, Had, Sak, Hap, KS };
+const char* teams_name[] = { "Natania", "Hadera", "Saknin", "Hapoel", "Kiryat Shmona" };
+
+// Global Vars
+int cnt_opt = 0, total_opt = 0;
+

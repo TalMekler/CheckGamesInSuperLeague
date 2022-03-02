c53642532a36b614104f72992319b798eddf674b
TalMekler <tal.mekler11@gmail.com>
2022-03-02 22:25:04 +0200

    Create Game.h

---
+++ Ligat_Al/check_infi/Game.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef GAME_DEBUG
+#define GAME_DEBUG
+#include "Header.h"
+
+
+typedef struct {
+	int home_id;
+	int guess_id;
+	int game_res; // 0 / 1 / 2
+} Game;
+
+#endif // !GAME_DEBUG

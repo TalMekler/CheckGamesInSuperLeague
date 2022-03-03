c5e03535a06b5504481ecf1c6fa9076a34ab6687
TalMekler <tal.mekler11@gmail.com>
2022-03-03 20:40:13 +0200

    Add teams name
    Add teams that are not fighting to be in playoff.
    *** If TEAMS == EXTRA_TEAM -> write it on the define!!! ***

--- Ligat_Al/check_infi/Header.h
+++ Ligat_Al/check_infi/Header.h
@@ -9,13 +9,14 @@
 
 //#define GAMES 7
 #define TEAMS 5
+#define EXTRA_TEAMS 9
 #define MAX_TEAM_IN_PLAYOFF 3
 
 // Global Vars
 extern int cnt_opt_, total_opt_;
-enum teams { Nat = 0, Had, Sak, Hap, KS };
+enum teams { Nat = 0, Had, Sak, Hap, KS, MTA, ASH, HJER, PTH};
 enum games_res_opt {Lose, Draw, Win = 3};
-extern const char* teams_name[TEAMS];
+extern const char* teams_name[EXTRA_TEAMS];
 //extern Game* games_arr[GAMES];
 extern int team_points[TEAMS];
 extern int GAMES;
--- Ligat_Al/check_infi/Main.c
+++ Ligat_Al/check_infi/Main.c
@@ -1,7 +1,7 @@
 #include "Team.h"
 
 int cnt_opt_ = 0, total_opt_ = 0;
-const char* teams_name[TEAMS] = { "Natania", "Hadera", "Saknin", "Hapoel", "Kiryat Shmona" };
+const char* teams_name[EXTRA_TEAMS] = { "Natania", "Hadera", "Saknin", "Hapoel TA", "Kiryat Shmona", "Maccabi TA", "Ashdod", "Hapoel Jer", "Petach Tikva"};
 //Game* games_arr[GAMES] = { 0 };
 int team_points[] = { 36,36,36,34,30 }; // Natania, Hadera, Saknin, Hapoel TA, Kiryat Shmona
 int GAMES = 0;
@@ -11,10 +11,10 @@ int main() {
 	Game g1 = { Nat, KS, 0 };
 	Game g2 = { Hap, Nat, 0 };
 	Game g3 = { Sak, Had, 0 };
-	Game g4 = { Had, -1, 0 };
-	Game g5 = { -1, Sak, 0 };
-	Game g6 = { -1, Hap, 0 };
-	Game g7 = { KS, -1, 0 };
+	Game g4 = { Had, MTA, 0 };
+	Game g5 = { PTH, Sak, 0 };
+	Game g6 = { HJER, Hap, 0 };
+	Game g7 = { KS, ASH, 0 };
 	Game* games_arr[] = { &g1,&g2,&g3,&g4,&g5,&g6,&g7 };
 	GAMES = sizeof(games_arr) / sizeof(Game*);
 	Team* teams_arr[TEAMS];
@@ -28,17 +28,5 @@ int main() {
 
 	printf("done!\n");
 
-
-	//Team t0 = { 0, 0 }; // natania 0
-	//Team t1 = { 1, 0 }; // hadera 1
-	//Team t2 = { 2, 0 }; // saknin 2
-	//Team t3 = { 3, 0 }; // hapoel 3
-	//Team t4 = { 4, 0 }; // kiryat shmona
-	//Team t_arr[] = { t0,t1,t2,t3,t4 };
-	//int arr[GAMES] = { 0 };
-	//func(arr, GAMES, t_arr);
-	//printf(">>> Games remined: %d <<<\n", GAMES);
-	//printf(">>> There are %d/%d combinations ", cnt_opt_, total_opt_); printPercent(((float)cnt_opt_ / total_opt_) * 100); printf(" <<<\n");
 	return 0;
-
 }

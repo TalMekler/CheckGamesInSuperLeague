0ef96474e4db843a7aa073ac64b034e6e56ead4f
TalMekler <tal.mekler11@gmail.com>
2022-03-19 20:58:21 +0200

    update teams points

--- Ligat_Al/check_infi/Main.c
+++ Ligat_Al/check_infi/Main.c
@@ -5,7 +5,7 @@ const char* teams_name[EXTRA_TEAMS] = { "Haifa", "Maccabi TA", "Beer Sheva", "Na
 //Game* games_arr[GAMES] = { 0 };
 int team_points[] = {	59, // Haifa
 						54, // Maccabi Tel Aviv
-						55, // Beer Sheva
+						58, // Beer Sheva
 						40, // Natania
 						43, // Saknin
 						38	// Hapoel Tel Aviv
@@ -17,8 +17,6 @@ int main() {
 
 	/* מחזור 1 */
 	Game g1_1 = { MHFC, HTA, 0 };
-	Game g1_2 = { HBS, Nat, 0 };
-	// Game g1_3 = { MTA, Sak, 0 };
 	
 	/* מחזור 2 */
 	Game g2_1 = { MHFC, HBS, 0 };
@@ -65,7 +63,7 @@ int main() {
 	Game g10_2 = { HBS, Sak, 0 };
 	Game g10_3 = { HTA, MTA, 0 };
 
-	Game* games_arr[] = { &g1_1, &g1_2/*, &g1_3*/, &g2_1, &g2_2, &g2_3, &g3_1, &g3_2, &g3_3, &g4_1, &g4_2, &g4_3
+	Game* games_arr[] = { &g1_1, &g2_1, &g2_2, &g2_3, &g3_1, &g3_2, &g3_3, &g4_1, &g4_2, &g4_3
 						,&g5_1, &g5_2, &g5_3, &g6_1, &g6_2,&g6_3, &g7_1, &g7_2, &g7_3, &g8_1, &g8_2, &g8_3,
 						&g9_1, &g9_2, &g9_3, &g10_1, &g10_2, &g10_3 };
 
